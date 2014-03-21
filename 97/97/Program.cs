6b3fb84b5162ba01d7c0688013c26be145553922
jmignosa <jmignosa@yahoo.com>
2014-03-21 15:42:09 -0400

    Problem 97
    Large non-Mersenne prime

---
+++ 97/97/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+
+namespace _97
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BigInteger result = 28433 * BigInteger.Pow(2, 7830457) + 1;
+            
+            Console.WriteLine("Large Non Mersenne Prime alst 10 digits {0}", result%10000000000);
+        }
+    }
+}
